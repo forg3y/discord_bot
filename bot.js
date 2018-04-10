43f215748413fbc615ecf30135e46a106c945d21
Trevor LeForge <36861613+forg3y@users.noreply.github.com>
2018-04-10 18:39:09 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,14 @@
 var Discord = require('discord.io');
-var logger = require('winston');
-
-// Configure logger settings (? google logger settings)
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-	colorize: true
-});
-logger.level = 'debug';
+// var logger = require('winston');
 
 // Initializez Discord Bot
 var bot = new Discord.Client();
 
-bot.on('ready', function(evt) {
-	logger.info('Connected');
-	logger.info('Logged in as: ');
-	logger.info(bot.username + ' - (' + bot.id + ')');
+bot.on('ready', () => {
+	console.log('?');
 });
-bot.on('message', function(user, userID, channelID, message, evt) {
+
+bot.on('message', () => {
 	// Our bot needs to know if it will execute a command
 	// it will listen for messages that will start with 'o3o '
 	if(message.substring(0,4) == 'o3o ') {
